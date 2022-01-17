51cccf5248038e5d71c39aae894d4dfdc8d0d6a9
smilejsu <smilejsu82@gmail.com>
2022-01-17 14:54:10 +0900

    벡터 덧셈과 뺄셈

--- HelloWorld/Assets/Scripts/App.cs
+++ HelloWorld/Assets/Scripts/App.cs
@@ -7,7 +7,10 @@ public class App : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        Vector2 playerPos = new Vector2(2, 3);
+        playerPos += new Vector2(8, 5);
+        Debug.Log(playerPos);
+
     }
 
     // Update is called once per frame
