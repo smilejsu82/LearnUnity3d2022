3ef85ef626d27ef947b2943cf315014eae5fe31e
smilejsu <smilejsu82@gmail.com>
2022-01-17 14:54:22 +0900

    벡터 덧셈과 뻴셈

--- HelloWorld/Assets/Scripts/App.cs
+++ HelloWorld/Assets/Scripts/App.cs
@@ -7,9 +7,20 @@ public class App : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Vector2 playerPos = new Vector2(2, 3);
+        //playerPos += new Vector2(8, 5);
+        //Debug.Log(playerPos);
+
         Vector2 playerPos = new Vector2(2, 3);
-        playerPos += new Vector2(8, 5);
-        Debug.Log(playerPos);
+        Vector2 monsterPos = new Vector2(5, 8);
+
+        var dir = monsterPos - playerPos;
+        var distance = dir.magnitude;
+        Debug.Log(distance);
+
+        //±Ê¿Ã 
+        //float distance = Vector2.Distance(playerPos, monsterPos);
+        //Debug.Log(distance);
 
     }
 
