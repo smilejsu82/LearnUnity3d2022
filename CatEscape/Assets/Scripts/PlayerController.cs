d291103fbdba3c8002f2048f965c597379e9dd09
smilejsu <smilejsu82@gmail.com>
2022-01-18 17:43:23 +0900

    플레이어 좌우이동시 화면 밖으로 나가는거 완료

--- CatEscape/Assets/Scripts/PlayerController.cs
+++ CatEscape/Assets/Scripts/PlayerController.cs
@@ -17,21 +17,42 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            this.transform.Translate(-3, 0, 0);
+
+            this.MoveLeft();
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            this.transform.Translate(3, 0, 0);
+            this.MoveRight();
+        }
+    }
+
+    private void MoveLeft() {
+        this.transform.Translate(-3, 0, 0);
+        if (this.transform.position.x <= -7.82f)
+        {
+            var pos = this.transform.position;
+            pos.x = -7.82f;
+            this.transform.position = pos;
+        }
+    }
+
+    private void MoveRight() {
+        this.transform.Translate(3, 0, 0);
+        if (this.transform.position.x >= 8.01f)
+        {
+            var pos = this.transform.position;
+            pos.x = 8.01f;
+            this.transform.position = pos;
         }
     }
 
     public void LButtonDown() {
-        this.transform.Translate(-3, 0, 0);
+        this.MoveLeft();
     }
 
     public void RButtonDown() {
-        this.transform.Translate(3, 0, 0);
+        this.MoveRight();
     }
 
     private void OnDrawGizmos()
