7a490ffb9273244e35bf61bd55983da47f33a9bf
smilejsu <smilejsu82@gmail.com>
2022-01-19 11:54:23 +0900

    [완료] 바운더리 체크해서 밖으로 못나가게 만듬

--- CatEscape2/CatEscape2/Assets/Scripts/PlayerController.cs
+++ CatEscape2/CatEscape2/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 1;
+    public Transform leftBoundaryPoint;
+    public Transform rightBoundaryPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +20,18 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             this.transform.Translate(Vector2.left * this.speed);
+            CheckBoundary();
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow)) 
         {
             this.transform.Translate(Vector2.right * this.speed);
+            CheckBoundary();
         }
+    }
+
+    private void CheckBoundary() {
+        var pos = this.transform.position;
+        pos.x = Mathf.Clamp(this.transform.position.x, this.leftBoundaryPoint.position.x, this.rightBoundaryPoint.position.x);
+        this.transform.position = pos;
     }
 }
