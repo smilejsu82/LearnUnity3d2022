1e017a8b41d64215e28400572e0b6dbbc8a8708b
smilejsu <smilejsu82@gmail.com>
2022-01-19 17:24:28 +0900

    로비를 만들고 케릭터를 선택해서 게임이 시작하도록 만든다 완료 ~

--- CatEscape2/CatEscape2/Assets/Scripts/GameMain.cs
+++ CatEscape2/CatEscape2/Assets/Scripts/GameMain.cs
@@ -6,19 +6,41 @@ using UnityEngine.SceneManagement;
 public class GameMain : MonoBehaviour
 {
     public static bool isGameOver = false;
-
     public PlayerController player;
     public UIGame uiGame;
     public UIGameOver uiGameOver;
     public ArrowGenerator arrowGenerator;
+    public Transform leftBoundary;
+    public Transform rightBoundary;
 
-    void Start()
+    public string[] prefabNames  = { "player_01", "player_02", "player_03"};
+    public Dictionary<string, GameObject> dicPrefabs = new Dictionary<string, GameObject>();
+
+    private void Awake()
     {
+        foreach (string prefabName in this.prefabNames) {
+            GameObject prefab = Resources.Load<GameObject>(prefabName);
+            dicPrefabs.Add(prefabName, prefab);
+        }
+        
+    }
+
+    public void Init(string prefabName) {
+
+        var targetPrefab = this.dicPrefabs[prefabName];
+
+        //프리팹을 인스턴스화 
+        GameObject playerGo = Instantiate(targetPrefab);
+
+        this.player = playerGo.AddComponent<PlayerController>();  //동적으로 컴포넌트를 부착 할수 있다 
+
+        this.player.Init(10, this.leftBoundary, this.rightBoundary, new Vector3(0, -3.68f, 0));
+
         //초기화 
         this.uiGameOver.btnRestart.onClick.AddListener(() => {
             //현재씬을 로드함 
             GameMain.isGameOver = false;
-            SceneManager.LoadScene("GameScene");
+            SceneManager.LoadScene("LobbyScene");
         });
 
         this.player.OnHit = (fillAmount) => {
@@ -26,7 +48,8 @@ public class GameMain : MonoBehaviour
         };
 
         this.player.OnDie = () => {
-            if (!GameMain.isGameOver) {
+            if (!GameMain.isGameOver)
+            {
                 GameMain.isGameOver = true;
                 Debug.Log("player is dead");
 
