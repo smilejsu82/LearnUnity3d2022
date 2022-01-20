d190f2c64eb6def6c36581aa0c8ba014a27f578c
smilejsu <smilejsu82@gmail.com>
2022-01-20 16:46:51 +0900

    플레이어 좌우 키보드 눌렀을때 이동하고 반전하기 완료

--- ClimbCloud/Assets/Scripts/PlayerController.cs
+++ ClimbCloud/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigid2d;
     public float jumpForce = 680f;
-
+    public float maxWalkSpeed = 2.0f;
+    public float walkSpeed = 30f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,20 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
             this.rigid2d.AddForce(Vector2.up * this.jumpForce);
         }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigid2d.velocity.x);
+
+        if (speedx < this.maxWalkSpeed) {
+            this.rigid2d.AddForce(this.transform.right * key * this.walkSpeed);
+        }
+
+        if (key != 0) {
+            this.transform.localScale = new Vector3(key, 1, 1);
+        }
+
     }
 }
