49936b906e8ea1e5fc4fe2a57fcffea5f1f5cb22
smilejsu <smilejsu82@gmail.com>
2022-01-21 09:37:26 +0900

    스프라이트 시트 애니메이션 테스트

--- ClimbCloud/Assets/Scripts/PlayerController.cs
+++ ClimbCloud/Assets/Scripts/PlayerController.cs
@@ -16,7 +16,6 @@ public class PlayerController : MonoBehaviour
         this.animator = this.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) {
