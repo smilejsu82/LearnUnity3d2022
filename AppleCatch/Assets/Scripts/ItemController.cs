16b613ff75b1d22df1bcb906372cd8157fa52b0b
smilejsu <smilejsu82@gmail.com>
2022-01-28 15:04:29 +0900

    아이템 드롭

---
+++ AppleCatch/Assets/Scripts/ItemController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.down * this.speed * Time.deltaTime);
+        if (this.transform.position.y < -1.0f) {
+            Destroy(this.gameObject);
+        }
+    }
+}
